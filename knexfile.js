21f759c610137ed35deb4a9fbd4f223b922c5d4b
Hill, Nicholas <NHill@barrick.com>
2018-01-30 14:26:54 -0800

    Update the variables to be global

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,23 @@
 const path = require('path');
 
+const host = process.env.POSTGRES_HOST || 'localhost';
+const port = process.env.POSTGRES_PORT || 5432;
+const user = process.env.POSTGRES_USER;
+const password = process.env.POSTGRES_PASSWORD;
+const database = process.env.POSTGRES_DB;
+
 const BASE_PATH = path.join(__dirname, 'src', 'server', 'db');
 
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://user:password@localhost:5432/demo_application',
+    connection: {
+      host,
+      port,
+      user,
+      password,
+      database,
+    },
     migrations: {
       directory: path.join(BASE_PATH, 'migrations')
     },
